adb23c7a779aabd971406f7697fd7da5815f7a2c
Egor Grushin <egormix1703@gmail.com>
2026-03-29 20:13:25 +0300

    + path update

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,7 +20,7 @@ app.get("/api/health", (_req, res) => {
 
 const clientDist = path.join(process.cwd(), "../client/dist");
 app.use(express.static(clientDist));
-app.get("*", (_req, res) => {
+app.get("/{*splat}", (_req, res) => {
     res.sendFile(path.join(clientDist, "index.html"));
 });
 
